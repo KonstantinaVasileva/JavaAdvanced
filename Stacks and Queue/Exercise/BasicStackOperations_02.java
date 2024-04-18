1a33ff369d9fcc9281bb112c172b450e0ac0d99b
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-04-18 16:54:14 +0300

    Add files via upload

---
+++ Stacks and Queue/Exercise/BasicStackOperations_02.java
@@ -0,0 +1,35 @@
+package Advanced.StacksAndQueues_Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BasicStackOperations_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] idx = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int n = idx[0];
+        int s = idx[1];
+        int x = idx[2];
+
+        int[] input = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            numbers.push(input[i]);
+        }
+        for (int i = 0; i < s; i++) {
+            numbers.pop();
+        }
+        if (numbers.contains(x)) {
+            System.out.println("true");
+        } else if (numbers.isEmpty()) {
+            System.out.println(0);
+        } else {
+            System.out.println(Collections.min(numbers));
+        }
+    }
+}
