d629fac9f7560eb87fdc7939291ccff06619ed40
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-06-15 07:50:54 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/OpinionPoll_01/Person.java
@@ -0,0 +1,19 @@
+package Advanced.DefiningClasses.Exercise.OpinionPoll_01;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
