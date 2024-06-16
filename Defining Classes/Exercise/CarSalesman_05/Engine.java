211b104324a761b7df1225b04b73ba67e6ad9409
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-06-16 14:32:18 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/CarSalesman_05/Engine.java
@@ -0,0 +1,31 @@
+package Advanced.DefiningClasses.Exercise.CarSalesman_05;
+
+public class Engine {
+    public String model;
+    public String power;
+    public String displacement;
+    public String efficiency;
+
+    public Engine(String model, String power, String displacement, String efficiency) {
+        this.model = model;
+        this.power = power;
+        this.displacement = displacement;
+        this.efficiency = efficiency;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getPower() {
+        return power;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+}
