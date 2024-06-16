461e7abfe21a49ee9d4c4d45735b83fb3c34ab53
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-06-16 14:31:07 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/CarSalesman_05/Car.java
@@ -0,0 +1,32 @@
+package Advanced.DefiningClasses.Exercise.CarSalesman_05;
+
+public class Car {
+    public String model;
+    public Engine engine;
+    public String weight;
+    public String color;
+
+    public Car(String model, Engine engine, String weight, String color) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+}
