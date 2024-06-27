4e3edeb544cb2a25be80a8d614344f912a2cef17
KonstantinaVasileva <150663482+KonstantinaVasileva@users.noreply.github.com>
2024-06-27 16:32:46 +0300

    Add files via upload

---
+++ Generics/Lab/JarOfT_01/Jar.java
@@ -0,0 +1,20 @@
+package Advanced.Generics.Lab.JarOfT_01;
+
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+    private ArrayDeque<T> stack;
+
+    public Jar() {
+        this.stack = new ArrayDeque<>();
+    }
+
+    public void add(T element) {
+        stack.push(element);
+    }
+
+    public T remove() {
+        return stack.pop();
+    }
+
+}
---
+++ Generics/Lab/JarOfT_01/Main.java
@@ -0,0 +1,9 @@
+package Advanced.Generics.Lab.JarOfT_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
